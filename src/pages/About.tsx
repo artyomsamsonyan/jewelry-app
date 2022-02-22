0e6f9d42de9ec23679e75b2c1895c53c08124f5a
artyom077 <artyom.samsonyan.dell@gmail.com>
2022-02-23 02:25:43 +0400

    second

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
-import { routes } from "../const";
 import { Layout } from "../layouts/Layout";
 
 export const About: React.FC = () => {
