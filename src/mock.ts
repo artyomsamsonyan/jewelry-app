9ca05d6eb3b593b427e345a854a04059039f13e7
artyom077 <artyom.samsonyan.dell@gmail.com>
2022-02-27 00:43:38 +0400

    fixed models sequence

--- src/mock.ts
+++ src/mock.ts
@@ -5,10 +5,8 @@ export const allModels = [
     "/assets/models/Matani_pechatka_3.jpg",
   ],
   [
-    "/assets/models/Titer_qarerov_1.jpg",
-    "/assets/models/Titer_qarerov_2.jpg",
-    "/assets/models/Titer_qarerov_3.jpg",
-    "/assets/models/Titer_qarerov_4.jpg",
+    "/assets/models/matani_zmruxtnerov_1.jpg",
+    "/assets/models/matani_zmruxtnerov_2.jpg",
   ],
   [
     "/assets/models/Matani_cep_1.jpg",
@@ -16,24 +14,22 @@ export const allModels = [
     "/assets/models/Matani_cep_3.jpg",
   ],
   [
-    "/assets/models/Matani_punj_1.jpg",
+    "/assets/models/Vardan_matani_uzor_1.jpg",
+    "/assets/models/Vardan_matani_uzor_2.jpg",
+    "/assets/models/Vardan_matani_uzor_3.jpg",
+    "/assets/models/Vardan_matani_uzor_4.jpg",
   ],
   [
-    "/assets/models/Tiffanny_key_1.jpg",
-    "/assets/models/Tiffanny_key_2.jpg",
-    "/assets/models/Tiffanny_key_3.jpg",
+    "/assets/models/Vardan_matani_safire_1.jpg",
+    "/assets/models/Vardan_matani_safire_2.jpg",
+    "/assets/models/Vardan_matani_safire_3.jpg",
+  ],
+  [
+    "/assets/models/Matani_punj_1.jpg",
   ],
   [
     "/assets/models/Vardan_matani_tiffany_1.jpg",
     "/assets/models/Vardan_matani_tiffany_2.jpg",
-  ],
-  [
-    "/assets/models/Xach_qarerov_1.jpg",
-    "/assets/models/Xach_qarerov_2.jpg",
-  ],
-  [
-    "/assets/models/Kulon_Emerald_NEW_REND_1.jpg",
-    "/assets/models/Kulon_Emerald_NEW_REND_2.jpg",
   ],
   [
     "/assets/models/R31L_1.jpg",
@@ -43,19 +39,23 @@ export const allModels = [
     "/assets/models/R31L_5.jpg",
   ],
   [
-    "/assets/models/Vardan_matani_safire_1.jpg",
-    "/assets/models/Vardan_matani_safire_2.jpg",
-    "/assets/models/Vardan_matani_safire_3.jpg",
+    "/assets/models/Titer_qarerov_1.jpg",
+    "/assets/models/Titer_qarerov_2.jpg",
+    "/assets/models/Titer_qarerov_3.jpg",
+    "/assets/models/Titer_qarerov_4.jpg",
   ],
   [
-    "/assets/models/matani_zmruxtnerov_1.jpg",
-    "/assets/models/matani_zmruxtnerov_2.jpg",
+    "/assets/models/Kulon_Emerald_NEW_REND_1.jpg",
+    "/assets/models/Kulon_Emerald_NEW_REND_2.jpg",
   ],
   [
-    "/assets/models/Vardan_matani_uzor_1.jpg",
-    "/assets/models/Vardan_matani_uzor_2.jpg",
-    "/assets/models/Vardan_matani_uzor_3.jpg",
-    "/assets/models/Vardan_matani_uzor_4.jpg",
+    "/assets/models/Tiffanny_key_1.jpg",
+    "/assets/models/Tiffanny_key_2.jpg",
+    "/assets/models/Tiffanny_key_3.jpg",
+  ],
+  [
+    "/assets/models/Xach_qarerov_1.jpg",
+    "/assets/models/Xach_qarerov_2.jpg",
   ],
 ];
 
@@ -65,7 +65,7 @@ export const mainModels = [
     number: 0,
   },
   {
-    src: "/assets/models/Titer_qarerov_1.jpg",
+    src: "/assets/models/matani_zmruxtnerov_1.jpg",
     number: 1,
   },
   {
@@ -73,39 +73,39 @@ export const mainModels = [
     number: 2,
   },
   {
-    src: "/assets/models/Matani_punj_1.jpg",
+    src: "/assets/models/Vardan_matani_uzor_1.jpg",
     number: 3,
   },
   {
-    src: "/assets/models/Tiffanny_key_1.jpg",
+    src: "/assets/models/Vardan_matani_safire_1.jpg",
     number: 4,
   },
   {
-    src: "/assets/models/Vardan_matani_tiffany_1.jpg",
+    src: "/assets/models/Matani_punj_1.jpg",
     number: 5,
   },
   {
-    src: "/assets/models/Xach_qarerov_1.jpg",
+    src: "/assets/models/Vardan_matani_tiffany_1.jpg",
     number: 6,
   },
   {
-    src: "/assets/models/Kulon_Emerald_NEW_REND_1.jpg",
+    src: "/assets/models/R31L_1.jpg",
     number: 7,
   },
   {
-    src: "/assets/models/R31L_1.jpg",
+    src: "/assets/models/Titer_qarerov_1.jpg",
     number: 8,
   },
   {
-    src: "/assets/models/Vardan_matani_safire_1.jpg",
+    src: "/assets/models/Kulon_Emerald_NEW_REND_1.jpg",
     number: 9,
   },
   {
-    src: "/assets/models/matani_zmruxtnerov_1.jpg",
+    src: "/assets/models/Tiffanny_key_1.jpg",
     number: 10,
   },
   {
-    src: "/assets/models/Vardan_matani_uzor_1.jpg",
+    src: "/assets/models/Xach_qarerov_1.jpg",
     number: 11,
   },
 ];
